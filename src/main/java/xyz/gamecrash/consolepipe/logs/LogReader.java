47159486948eb147919eb98b436f0355eb21295b
game.crash <73080127+gamecrash534@users.noreply.github.com>
2025-07-18 21:50:42 +0200

    Add GZip support (finally) and put search results to a minimessage click thingy

--- src/main/java/xyz/gamecrash/consolepipe/logs/LogReader.java
+++ src/main/java/xyz/gamecrash/consolepipe/logs/LogReader.java
@@ -1,11 +1,14 @@
 package xyz.gamecrash.consolepipe.logs;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
 
 public class LogReader {
     private final Log log;
@@ -13,7 +16,14 @@ public class LogReader {
     public LogReader(Log log) { this.log = log; }
 
     public List<String> getAllLines() throws IOException {
-        return Files.readAllLines(Paths.get(log.getPath()));
+        if (log.getPath().endsWith(".gz")) {
+            try (GZIPInputStream gzip = new GZIPInputStream(Files.newInputStream(Paths.get(log.getPath())));
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(gzip))) {
+                return reader.lines().collect(Collectors.toList());
+            }
+        } else {
+            return Files.readAllLines(Paths.get(log.getPath()));
+        }
     }
 
     public String readLine(int lineNumber) throws IOException {
