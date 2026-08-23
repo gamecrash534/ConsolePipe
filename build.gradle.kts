144bd27d72ff2145603df1779d0e90e1d242156c
game.crash <73080127+gamecrash534@users.noreply.github.com>
2026-08-24 00:06:03 +0200

    git init

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import java.net.URI
+
+plugins {
+    id("java-library")
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.22"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven { url = uri("https://repo.codemc.io/repository/maven-releases/") }
+}
+
+dependencies {
+    paperweight.paperDevBundle("26.2.build.+")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("26.2")
+        jvmArgs("-Xms2G", "-Xmx2G")
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+}
